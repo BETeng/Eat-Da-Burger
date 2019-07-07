5da05799b78b1dd9daa1bb1ef59d288ebcc1b249
BETeng <brian7.teng@gmail.com>
2019-07-07 16:45:19 -0500

    created wireframe and db schema seed

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheezeburger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Chickenburger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible-burger');
+INSERT INTO burgers (burger_name) VALUES ('SadBurger');
+INSERT INTO burgers (burger_name) VALUES ('IceBurgerLettuce');
